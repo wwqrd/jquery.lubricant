b08a8e9541574b816d8bdc9d73d45fcb5efd3e38
steeeve <stevemckellar@gmail.com>
2012-05-03 19:02:56 +0100

    All in good order

--- src/jquery.lubricant.js
+++ src/jquery.lubricant.js
@@ -1,9 +1,13 @@
 ;(function ( $, window, undefined ) {
     
-    var pluginName = 'defaultPluginName',
+    var pluginName = 'lubricant',
         document = window.document,
         defaults = {
-            propertyName: "value"
+            depth: 1,
+            scrollX : true,
+            scrollY: true,
+            tween: 'parallax',
+            zOffset: 1000
         };
 
     function Plugin( element, options ) {
@@ -17,7 +21,67 @@
     }
 
     Plugin.prototype.init = function () {
-        // code goes here
+        var self = this;
+
+        this.width = $(this.element).width();
+        this.height = $(this.element).height();
+        this.origin = $(this.element).offset();
+
+        $(window).bind('scroll',function(e) {
+            self.positionElements(e);
+        });
+
+        $(window).bind('resize',function(e) {
+            self.updateViewSettings();
+            self.positionElements(e);
+        });
+
+        this.updateViewSettings();
+
+        $(this.element).css({'zIndex': Math.round(this.options.zOffset + this.options.depth*1000)})
+    };
+
+    Plugin.prototype.updateViewSettings = function() {
+        this.view = {
+            'width': $(window).width(),
+            'height': $(window).height()
+        };
+        this.maxDistance = {
+            'x': (this.view.width/2)+this.width*0.5,
+            'y': (this.view.height/2)+this.height*0.5
+        };
+    };
+
+    Plugin.prototype.getOffset = function() {
+        var distanceFromCentre = {
+            'x': this.origin.left - $(window).scrollLeft() - (this.view.width-this.height)/2,
+            'y': this.origin.top - $(window).scrollTop() - (this.view.height-this.width)/2
+        };
+
+        var difference = {
+            'x': distanceFromCentre.x/this.maxDistance.x,
+            'y': distanceFromCentre.y/this.maxDistance.y
+        };
+
+        switch(this.options.tween) {
+            case 'parallax':
+            default:
+                var offset = {
+                    'top': difference.y*(this.options.depth-1)*this.maxDistance.y,
+                    'left': difference.x*(this.options.depth-1)*this.maxDistance.x
+                };
+        }
+        
+        return offset;
+    };
+
+    Plugin.prototype.positionElements = function(e) {
+        var offset = this.getOffset();
+
+        $(this.element).css({
+            'marginTop': offset.top,
+            'marginLeft': offset.left
+        });
     };
 
     $.fn[pluginName] = function ( options ) {
